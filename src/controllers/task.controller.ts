ab58bfc80a37064161c06db6b5b24f9813182d81
unknown <matheusczeckcz@gmail.com>
2026-06-14 22:55:06 -0300

    creating update and delete in controller task

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -57,27 +57,54 @@ class TaskController {
     }
   }
 
-  static async update(req: Request, res: Response):Promise<Response>{
-    try{
-        const {id} = req.params;
-        const { title, description, dueDate, status, priority, categoryId } = req.body;
+  static async update(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
+      const { title, description, dueDate, status, priority, categoryId } =
+        req.body;
 
-        const task = await Task.findByPk(id);
+      const task = await Task.findByPk(Number(id));
 
-        if(!task){
-            return res.status(404).json({ error: "Tarefa nao encontrada."})
-        }
+      if (!task) {
+        return res.status(404).json({ error: "Tarefa nao encontrada." });
+      }
 
-        await task.update({
-            title: title ?? task.title,
-            description: description ?? task.description,
-            dueDate: dueDate ?? task.dueDate,
-            status: status ?? task.status,
-            priority: priority ?? task.priority,
-            categoryId: categoryId ?? task.categoryId,
+      await task.update({
+        title: title ?? task.title,
+        description: description ?? task.description,
+        dueDate: dueDate ?? task.dueDate,
+        status: status ?? task.status,
+        priority: priority ?? task.priority,
+        categoryId: categoryId ?? task.categoryId,
+      });
+      return res
+        .status(200)
+        .json(`A tarefa: ${task.title} foi alterada com sucesso!`);
+    } catch (error: any) {
+      return res.status(500).json({
+        error: "Erro interno do servidor",
+        details: error.message,
+      });
+    }
+  }
 
-        })
+  static async delete(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
 
+      const task = await Task.findByPk(Number(id));
+
+      if (!task) {
+        return res.status(404).json("Tarefa não encontrada!");
+      }
+
+      await task.destroy();
+      return res.status(200).json(`Tarefa ${task.title} excluida com sucesso!`);
+    } catch (error: any) {
+      return res.status(500).json({
+        error: "Erro interno do servido!",
+        details: error.message,
+      });
     }
   }
 }
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get("/:userId", TaskController.listByUser);
 router.post("/", TaskController.create);
+router.put("/updateTask/:id", TaskController.update);
+router.delete("/delete/:id", TaskController.delete);
 
 export default router;
