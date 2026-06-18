692a66ba85bf9f6f13f1865ae23b20ad4b0d2990
unknown <matheusczeckcz@gmail.com>
2026-06-17 22:24:12 -0300

    downgrade noje.js version

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import categoryRoutes from "./routes/category.routes.ts";
 import taskyRoutes from "./routes/task.routes.ts";
 import userRoutes from "./routes/user.routes.ts";
+import cors from "cors";
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/categories", categoryRoutes);
 app.use("/task", taskyRoutes);
